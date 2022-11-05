18c4e806e540b3b13e4400b5d3b1c5de88dac972
sean-code <john.nganga@student.moringaschool.com>
2022-11-05 12:44:48 +0300

    Run migration for the review

--- db/migrate/20221105085220_create_reviews.rb
+++ db/migrate/20221105085220_create_reviews.rb
@@ -1,4 +1,13 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.integer :product_id
+      t.integer :user_id
+      t.integer :star_rating
+      t.string :comment
+    
+      t.timestamps
+    end
+    
   end
 end
