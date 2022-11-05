608998716fef720dd4a2898a7c17270497d6a68c
sean-code <john.nganga@student.moringaschool.com>
2022-11-05 14:44:10 +0300

    Add the aggregate association methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,19 @@
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+        self.reviews.create(user: user, star_rating: star_rating, comment: comment) #takes a User (an instance of the User class), a star_rating (integer), and a comment (string) as arguments, and creates a new Review in the database associated with this Product and the User
+    end
+
+    def print_all_reviews 
+        self.reviews.each do |revi|
+            puts "Review for #{revi.product.name} by #{revi.user.name}: #{revi.star_rating}. #{revi.comment}" #puts to the terminal a string representing each review for this product
+        end
+    end
+
+    def average_rating
+        self.reviews.average(:star_rating) #returns a float representing the average star rating for all reviews for this product
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
+    
+    # def all_reviews
+    #     self.reviews.all 
+    # end
+
+    # def all_products
+    #     self.products.all
+    # end
+
+    def remove_reviews(product)
+        self.find(product_id).reviews.destroy_all
+    end
+
+    def favorite_product
+        self.reviews.all.sort{ |one,two| one.star_rating <=> two.star_rating }.last #returns the product instance that has the highest star rating from this user
+    end
 end
