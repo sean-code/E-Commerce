5c5e80a7ab59cc6c8669937f03ad7e1656948a57
sean-code <john.nganga@student.moringaschool.com>
2022-11-05 13:37:42 +0300

    Add foreign keys and reference them in Reviews

--- db/migrate/20221105085220_create_reviews.rb
+++ db/migrate/20221105085220_create_reviews.rb
@@ -1,8 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-      t.integer :product_id
-      t.integer :user_id
+      t.references :product, foreign_key: true
+      t.references :user, foreign_key: true
       t.integer :star_rating
       t.string :comment
     
